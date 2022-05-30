45b7198060bdc70e37ca9ff7894829c24d6b96a6
ayman-alshantaf <ayman1998.alshantaf@gmail.com>
2022-05-30 21:43:47 +0300

    update

---
+++ js/mainScript.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+    $('.slider-congress').slick({
+        rtl: true,
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        variableWidth: true,
+        responsive: [
+            {
+                breakpoint: 1000,
+                settings: {
+                    arrows: false,
+                    slidesToShow: 1,
+                    variableWidth: false,
+
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    arrows: false,
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+
+})
