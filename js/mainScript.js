b014d4304461494eebac510ecee56f0826d558b0
ayman-alshantaf <ayman1998.alshantaf@gmail.com>
2022-06-05 19:28:11 +0300

    u

--- js/mainScript.js
+++ js/mainScript.js
@@ -24,5 +24,65 @@ $(document).ready(function() {
             }
         ]
     });
+    $('.table-results .slider-result').slick({
+        rtl: true,
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        variableWidth: true,
+        responsive: [
+            {
+                breakpoint: 1000,
+                settings: {
+                    arrows: false,
+                    slidesToShow: 1,
 
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    arrows: false,
+                    slidesToShow: 1,
+                }
+            }
+        ]
+    });
+    $('.card-results .all-slider').slick({
+        rtl: true,
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        variableWidth: true,
+        responsive: [
+            {
+                breakpoint: 1000,
+                settings: {
+                    arrows: false,
+                    slidesToShow: 1,
+
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    arrows: false,
+                    slidesToShow: 1,
+                }
+            }
+        ]
+    });
+
+    $('.table-body').on('click' , function (){
+        $(this).siblings().find(".body-table").slideUp("slow");
+        $(this).find(".body-table").slideDown("slow");
+        $(this).find(".fa").addClass("fa-chevron-down");
+        $(this).siblings().find(".fa").addClass("fa-chevron-up")
+        $(this).siblings().find(".fa").removeClass("fa-chevron-down")
+
+    })
 })
